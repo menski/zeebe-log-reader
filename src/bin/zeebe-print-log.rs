e7e07af88f154c5a6c2a972d145fc5878f1dabcd
Sebastian Menski <sebastian.menski@camunda.com>
2017-12-14 13:55:57 +0100

    chore(zeebe-print-log): convert every log file separately

--- src/bin/zeebe-print-log.rs
+++ src/bin/zeebe-print-log.rs
@@ -9,21 +9,27 @@ use std::time::Instant;
 
 use zeebe_log_reader::LogStream;
 
-const OUTPUT_FILE: &str = "log.txt";
-
 fn main() {
     let now = Instant::now();
     match try_main() {
-        Ok(_) => println!("Log written to file {} in {:?}", OUTPUT_FILE, now.elapsed()),
+        Ok(_) => {
+            let duration = now.elapsed();
+            println!("Took {}.{:09}s", duration.as_secs(), duration.subsec_nanos())
+        },
         Err(e) => eprintln!("Error: {}", e),
     }
 }
 
 fn try_main() -> Result<(), Error> {
-    let mut output = BufWriter::new(File::create(OUTPUT_FILE)?);
 
     for filename in std::env::args().skip(1) {
+        let now = Instant::now();
+
+        let output_name = format!("{}.{}", filename, "txt");
+        print!("Converting {} to {}", filename, output_name);
+
         let mut file = File::open(&filename)?;
+        let mut output = BufWriter::new(File::create(output_name)?);
 
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer)?;
@@ -33,6 +39,9 @@ fn try_main() -> Result<(), Error> {
         for event in logstream {
             writeln!(output, "{:?}", event)?;
         }
+
+        let duration = now.elapsed();
+        println!(". Took {}.{:09}s", duration.as_secs(), duration.subsec_nanos());
     }
 
     Ok(())

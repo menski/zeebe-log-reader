8e6d20faefc1578d2d7fa553374bafd0e0e9c2cf
Sebastian Menski <sebastian.menski@camunda.com>
2017-12-15 14:29:21 +0100

    wip

--- src/bin/zeebe-convert-log.rs
+++ src/bin/zeebe-convert-log.rs
@@ -1,13 +1,31 @@
+#[macro_use]
 extern crate failure;
 extern crate zeebe_log_reader;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
 
 use failure::Error;
 use std::fs::File;
-use std::io::BufWriter;
 use std::io::prelude::*;
 use std::time::Instant;
+use std::convert;
 
-use zeebe_log_reader::LogStream;
+use structopt::StructOpt;
+
+use zeebe_log_reader::{LogStream, EventType};
+use zeebe_log_reader::output::*;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "zeebe-convert-log", about = "Convert Zeebe log streams to plain text output")]
+struct Opt {
+    #[structopt(short = "c", long = "console", help = "Output to console instead of file")]
+    console: bool,
+    #[structopt(help = "Input files")]
+    input: Vec<String>,
+    #[structopt(short = "f", long = "filter", help = "Filter event type")]
+    filter: Option<EventType>,
+}
 
 fn main() {
     let now = Instant::now();
@@ -20,16 +38,23 @@ fn main() {
     }
 }
 
+
 fn try_main() -> Result<(), Error> {
+    let opt = Opt::from_args();
 
-    for filename in std::env::args().skip(1) {
+    let mut output: Box<EventOutput> = Box::new(StdOutput::new());
+
+    for filename in opt.input.iter() {
         let now = Instant::now();
-
-        let output_name = format!("{}.{}", filename, "txt");
-        print!("Converting {} to {}", filename, output_name);
-
         let mut file = File::open(&filename)?;
-        let mut output = BufWriter::new(File::create(output_name)?);
+
+        if !opt.console {
+            let output_name = format!("{}.{}", filename, "txt");
+            print!("Converting {} to {}", filename, output_name);
+
+            output = Box::new(FileOutput::new(&output_name)?);
+        }
+
 
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer)?;
@@ -37,11 +62,15 @@ fn try_main() -> Result<(), Error> {
         let logstream = LogStream::new(&buffer)?;
 
         for event in logstream {
-            writeln!(output, "{:?}", event)?;
+            if let Some(event_type) = opt.filter {
+            }
+            output.output(&event)?;
         }
 
-        let duration = now.elapsed();
-        println!(". Took {}.{:09}s", duration.as_secs(), duration.subsec_nanos());
+        if !opt.console {
+            let duration = now.elapsed();
+            println!(". Took {}.{:09}s", duration.as_secs(), duration.subsec_nanos());
+        }
     }
 
     Ok(())
---
+++ src/output.rs
@@ -0,0 +1,45 @@
+use LogEvent;
+use std::io::{self, BufWriter};
+use std::fs::File;
+use std::io::prelude::*;
+use failure::Error;
+
+pub trait EventOutput {
+    fn output(&mut self, event: &LogEvent) -> Result<(), Error>;
+}
+
+pub struct StdOutput {
+    output: io::Stdout,
+}
+
+impl StdOutput {
+    pub fn new() -> Self {
+        StdOutput {
+            output: io::stdout(),
+        }
+    }
+}
+
+impl EventOutput for StdOutput {
+    fn output(&mut self, event: &LogEvent) -> Result<(), Error> {
+        Ok(writeln!(self.output, "{:?}", event)?)
+    }
+}
+
+pub struct FileOutput {
+    output: BufWriter<File>,
+}
+
+impl FileOutput {
+    pub fn new(filename: &str) -> Result<Self, Error> {
+        Ok(FileOutput {
+            output: BufWriter::new(File::create(filename)?),
+        })
+    }
+}
+
+impl EventOutput for FileOutput {
+    fn output(&mut self, event: &LogEvent) -> Result<(), Error> {
+        Ok(writeln!(self.output, "{:?}", event)?)
+    }
+}
